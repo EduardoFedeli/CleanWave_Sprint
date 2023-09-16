b4fb5686e012c0626022d34234191c906955a9b8
EduardoFedeli <dudufedeli@outlook.com>
2023-09-16 19:25:44 -0300

    Arrumando o grid-container

--- src/Components/Body.jsx
+++ src/Components/Body.jsx
@@ -8,66 +8,78 @@ import videoSource from './Assets/videos/video.mp4';
 function Body() {
   return (
     <main>
-      <section className="product-section">
-            <h1>Produtos</h1>
-        
-            <article className="product">
-          
-                <header>
-            
-                    <h2>Vaso Smart</h2>
-                    <img src={vasoImage} alt="Vaso" style={{ width: '300px', height: '300px' }} />
-          
-                </header>
-          
-                <p>Descrição do Produto 1...</p>
-          
-                 <footer>
-                    <p>Preço: R$ 100,00</p>
-                </footer>
-        
-            </article>
-
-            <article className="product">
-          
-                <header>
-            
-                    <h2>Nome do Produto 2</h2>
-                    <img src={vasoImage2} alt="Vaso" style={{ width: '300px', height: '300px' }} />
-          
-                </header>
-          
-                <p>Descrição do Produto 2...</p>
-          
-                <footer>
-                    <p>Preço: R$ 120,00</p>
-                </footer>
-        
-        
-            </article>
-
-        <article className="product">
-          
-            <header>
-            
-                <h2>Nome do Produto 3</h2>
-                <img src={vasoImage3} alt="Vaso" style={{ width: '300px', height: '300px' }} />
-
-            </header>
-          
-            <p>Descrição do Produto 3...</p>
-          
-            <footer>
-                <p>Preço: R$ 80,00</p>
-            </footer>
-        
-        </article>
-      </section>
-
+      
+      
       <div className="grid-container">
-        <div className="grid-item">Item 1</div>
-        <div className="grid-item">Item 2</div>
-        <div className="grid-item">Item 3</div>
+            <section className="product-section">
+                    <h1>Produtos</h1>
+                    
+                    
+                    <div className="grid-item">Item 1
+                        
+                        <article className="product">
+          
+                            <header>
+            
+                                <h2>Vaso Smart</h2>
+                                <img src={vasoImage} alt="Vaso" style={{ width: '300px', height: '300px' }} />
+          
+                            </header>
+          
+                            <p>Descrição do Produto 1...</p>
+          
+                            <footer>
+                                <p>Preço: R$ 100,00</p>
+                            </footer>
+        
+                        </article>
+                    
+                    </div>
+                    
+                    <div className="grid-item">Item 2    
+                        
+                        <article className="product">
+          
+                            <header>
+            
+                                <h2>Nome do Produto 2</h2>
+                                <img src={vasoImage2} alt="Vaso" style={{ width: '300px', height: '300px' }} />
+          
+                             </header>
+          
+                            <p>Descrição do Produto 2...</p>
+          
+                            <footer>
+                                <p>Preço: R$ 120,00</p>
+                            </footer>
+                
+        
+                        </article>
+                    
+                    </div>
+                    
+                    <div className="grid-item">Item 3   
+                        
+                        <article className="product">
+          
+                            <header>
+            
+                                <h2>Nome do Produto 3</h2>
+                                <img src={vasoImage3} alt="Vaso" style={{ width: '300px', height: '300px' }} />
+
+                            </header>
+          
+                            <p>Descrição do Produto 3...</p>
+          
+                            <footer>
+                                <p>Preço: R$ 80,00</p>
+                            </footer>
+        
+                        </article>
+                    
+                    </div>
+            
+            </section>
       </div>
 
 
@@ -75,10 +87,7 @@ function Body() {
             <source src={videoSource} type="video/mp4" style={{ width: '350px', height: '200px' }} />""
             Seu navegador não suporta o elemento de vídeo.
         </video>
-
-
-
-
+    
     </main>
   );
 }
