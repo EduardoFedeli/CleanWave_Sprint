db2f1e9afb108ac6024e483f7f64db8b54b22137
jessiebrum <rm97944@fiap.com.br>
2023-09-16 21:28:10 -0300

    Mudanças no layout

--- src/Components/Styles.jsx
+++ src/Components/Styles.jsx
@@ -8,7 +8,7 @@ export const FullPage = styled.div`
   flex-direction: column;
   align-items: center;
   justify-content: center;
-  background: linear-gradient(to bottom, #0074cc, #3d6cb3, #003f87);
+  background: linear-gradient(to bottom, #0074cc,  #3d6cb3, #003f87);
   font-weight: bold;
   font-size: 20px;
   text-align: center;
